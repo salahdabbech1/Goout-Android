b4325bbbd708fff4daca23473853c5204d62989f
salahdabbech1 <salah.dabbech@esprit.tn>
2022-04-05 09:34:20 +0100

    login and register added in interface

--- app/src/main/java/com/example/goout/interface/ParentApiInterface.kt
+++ app/src/main/java/com/example/goout/interface/ParentApiInterface.kt
@@ -5,10 +5,15 @@ import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.GET
 import com.example.goout.model.Parent
 import retrofit2.Call
+import retrofit2.http.POST
 
 interface ParentApiInterface {
-    @GET("/")
+    @GET("/getall")
     fun getparents() : Call<List<Parent>>
+    @POST("/Loginparent")
+    fun login(): Call<Parent>
+    @POST("RegisterParent")
+    fun register():Call<Parent>
 
     companion object {
 
